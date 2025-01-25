7284f970fcfed2e2d9cbe77ef37fe91171fadc59
Eswara Prasath M <91394348+pechaathu@users.noreply.github.com>
2025-01-25 21:10:07 +0530

    Add files via upload

--- Stock_Analysis.py
+++ Stock_Analysis.py
@@ -501,6 +501,7 @@ def profitloss(content, industry):
             barchartcol, waterfallcol = st.columns([1.5,1])
             with barchartcol:
                 filtered_data_df = dfrequired[dfrequired["Type"]==resultoption]
+                filtered_data_df
                 values = filtered_data_df.iloc[0,:-1]
                 intvalues = [item for item in values if not isinstance(item, str)]
                 ymin = min(list(intvalues))*1.25 if min(list(intvalues))<0 else 0
